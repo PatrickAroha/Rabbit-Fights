c573210b0874947b299310d8bbe8a8720ce6e31d
FounderHA <rafaeumn@gmail.com>
2026-02-20 21:04:48 -0300

    att lobby status ready

---
+++ Source/Deep/Core/Lobby/MyPlayerControllerLobby.cpp
@@ -0,0 +1,47 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyPlayerControllerLobby.h"
+#include "MyHUDLobby.h"
+
+void AMyPlayerControllerLobby::InitPlayerState()
+{
+	Super::InitPlayerState();
+}
+
+void AMyPlayerControllerLobby::SpawnDefaultHUD()
+{
+	Super::SpawnDefaultHUD();
+}
+
+void AMyPlayerControllerLobby::OnRep_PlayerState()
+{
+	Super::OnRep_PlayerState();
+	
+	if (IsLocalController())
+	{
+		AMyHUDLobby* MyHud = GetHUD<AMyHUDLobby>();
+		if (MyHud != nullptr)
+		{
+		
+			MyHud->CreateHudLobby(PlayerState);
+		}
+	}
+}
+
+void AMyPlayerControllerLobby::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (HasAuthority() && IsLocalController())
+	{
+		AMyHUDLobby* MyHud = GetHUD<AMyHUDLobby>();
+		if (MyHud != nullptr)
+		{
+			MyHud->CreateHudLobby(PlayerState);
+		}
+	}
+
+}
+
+
