73df04b403c5c090c628d1cb1b6dff1ed9769bbc
Thayllor <thayllorsander@gmail.com>
2026-02-23 16:54:05 -0300

    Resolvendo BO

--- Source/Deep/Core/Lobby/MyPlayerStateLobby.cpp
+++ Source/Deep/Core/Lobby/MyPlayerStateLobby.cpp
@@ -1,7 +1,7 @@
 ﻿// Fill out your copyright notice in the Description page of Project Settings.
 
-#include "MyGameModeLobby.h"
 #include "MyPlayerStateLobby.h"
+#include "MyGameModeLobby.h"
 #include "MyHUDLobby.h"
 #include "Net/UnrealNetwork.h"
 
