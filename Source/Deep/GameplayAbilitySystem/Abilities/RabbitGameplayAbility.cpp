f8f21a54fd31a4a21fe0b1a87e6703ab6619ce90
PatrickAroha <patrickaroha3@gmail.com>
2026-07-22 16:46:06 -0300

    Fixing Bugs versão 2

--- Source/Deep/GameplayAbilitySystem/Abilities/RabbitGameplayAbility.cpp
+++ Source/Deep/GameplayAbilitySystem/Abilities/RabbitGameplayAbility.cpp
@@ -2,3 +2,28 @@
 
 
 #include "RabbitGameplayAbility.h"
+
+void URabbitGameplayAbility::Tick(float DeltaTime)
+{
+	TickGA(DeltaTime);
+}
+
+ETickableTickType URabbitGameplayAbility::GetTickableTickType() const
+{
+	return HasAnyFlags(RF_ClassDefaultObject) ? ETickableTickType::Never : ETickableTickType::Always;
+}
+
+bool URabbitGameplayAbility::IsTickable() const
+{
+	return IsTickEnabled;
+}
+
+TStatId URabbitGameplayAbility::GetStatId() const
+{
+	RETURN_QUICK_DECLARE_CYCLE_STAT(URabbitGameplayAbility, STATGROUP_Tickables);
+}
+
+void URabbitGameplayAbility::SetEnableTick(bool Enable)
+{
+	IsTickEnabled = Enable;
+}
--- Source/Deep/GameplayAbilitySystem/Abilities/RabbitGameplayAbility.h
+++ Source/Deep/GameplayAbilitySystem/Abilities/RabbitGameplayAbility.h
@@ -6,12 +6,25 @@
 #include "RabbitGameplayAbility.generated.h"
 
 UCLASS()
-class DEEP_API URabbitGameplayAbility : public UGameplayAbility
+class DEEP_API URabbitGameplayAbility : public UGameplayAbility, public FTickableGameObject
 {
 	GENERATED_BODY()
 
 public:
+	virtual void Tick(float DeltaTime) override;
+	virtual ETickableTickType GetTickableTickType() const override;
+	virtual bool IsTickable() const override;
+	virtual TStatId GetStatId() const override;
+
+	UFUNCTION(BlueprintCallable)
+	void SetEnableTick(bool Enable);
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void TickGA(float DeltaTime);
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "UI")
 	bool ShouldShowInAbilitiesBar = false;
+
+	UPROPERTY(EditAnywhere, Category = "Tick")
+	bool IsTickEnabled = false;
 };
